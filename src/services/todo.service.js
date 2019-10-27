e587d87c61fe22f33444d47d196dc8df2bb10243
Eric Lara <eric.lara@axa.com>
2019-10-27 17:04:08 +0100

    feat(fetch): add remote content

--- src/services/todo.service.js
+++ src/services/todo.service.js
@@ -1,12 +1,31 @@
 import itemList from '../settings';
 
 class TodoService {
-   getItems() {
+   getMockItems() {
       return new Promise((resolve, reject) => {
          setTimeout(() => {
             resolve(itemList);
          }, 3000);
       });
+   }
+
+   async getItems() {
+      const items = await this.getBooks('nodejs', 3);
+
+      return items.map(item => {
+         return {
+            id: item.id,
+            text: item.volumeInfo.title,
+            value: item.id
+         }
+      });
+   }
+
+   async getBooks(query, limit) {
+      const response = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${query}`);
+      const parsedResult = await response.json();
+
+      return parsedResult.items.slice(0, limit);
    }
 }
 
